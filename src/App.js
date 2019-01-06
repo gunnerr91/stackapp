10c78d4f9f627d7d106fb94c8252665662d4c269
gunnerr91 <blissvortex@hotmail.com>
2019-01-06 19:40:11 +0000

    added shared files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers/Index";
+import Header from "./components/shared/Header";
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header header="Stack app" />
+      </View>
     </Provider>
   );
 };
